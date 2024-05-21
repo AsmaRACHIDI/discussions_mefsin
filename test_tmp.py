77040d1e9542d5deb61f1a5daf00843910e647b6
Asma RACHIDI <rachidi.asm@gmail.com>
2024-05-21 17:24:37 +0200

    feat: ajout fonction et  test fetch_disucssions_from_data_eco_api

--- test_tmp.py
+++ test_tmp.py
@@ -1,27 +1,50 @@
 import pytest
-from tmp import fetch_data_from_data_gouv_api, fetch_data_from_data_eco_api, format_data
+import os
+from dotenv import load_dotenv
+
+from tmp import fetch_discussions_from_data_gouv_api, fetch_datasets_from_data_eco_api, fetch_discussions_from_data_eco_api, format_data
+
+# Récupération des variables d'environnement
+load_dotenv()
 
 @pytest.fixture
 def data_gouv_api_url():
     return "https://www.data.gouv.fr/api/1/organizations/ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique/discussions"
 
 @pytest.fixture
-def data_eco_api_url():
+def data_eco_datasets_api_url():
     return "https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets"
+
+@pytest.fixture
+def data_eco_discussions_api_url():
+    return "https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/interne-discussions/records"
+
 
 # def test_fetch_data_from_data_gouv_api(data_gouv_api_url):
     # data = fetch_data_from_data_gouv_api(data_gouv_api_url)
-def test_fetch_data_from_data_gouv_api():
-    data = fetch_data_from_data_gouv_api()
+def test_fetch_discussions_from_data_gouv_api():
+    data = fetch_discussions_from_data_gouv_api()
     assert isinstance(data, list)
     assert len(data) > 0
 
+
 #def test_fetch_data_from_data_eco_api(data_eco_api_url):
     #data = fetch_data_from_data_eco_api(data_eco_api_url)
-def test_fetch_data_from_data_eco_api():
-    data = fetch_data_from_data_eco_api()
+def test_fetch_datasets_from_data_eco_api():
+    data = fetch_datasets_from_data_eco_api()
     assert isinstance(data, list)
     assert len(data) > 0
+
+
+
+def test_fetch_discussions_from_data_eco_api():
+    data = fetch_discussions_from_data_eco_api()
+    assert isinstance(data, dict)
+    assert "records" in data
+    assert "total_count" in data
+    assert len(data["records"]) == data["total_count"]
+    print(len(data["records"]))
+
 
 def test_format_data_data_gouv():
     data_json = [
--- tmp.py
+++ tmp.py
@@ -1,9 +1,17 @@
 import json
 import requests
+import os
 
+from dotenv import load_dotenv
 from format import dump_json, append_to_csv
 
-def fetch_data_from_data_gouv_api() -> dict:
+load_dotenv()
+
+# Clé d'API
+HEADERS = {"Authorization": f"Apikey {os.environ['KEY']}"}
+
+
+def fetch_discussions_from_data_gouv_api() -> dict:
 #def fetch_data_from_data_gouv_api(api_url: str, page: int = 1) -> dict:
     """
     Récupère les discussions à partir de l'API de data.gouv.fr
@@ -14,15 +22,16 @@ def fetch_data_from_data_gouv_api() -> dict:
         response = requests.get(api_url)
         response.raise_for_status()  # Lève une exception si la requête échoue
         data_json = response.json()
+        print(data_json)
         # Sauvegarde les discussions formatées au format JSON
-        dump_json("all_data_gouv_datasets.json", data_json)
+        dump_json("all_data_gouv_discussions.json", data_json)
         return data_json
     except requests.RequestException as e:
         print(f"Erreur lors de la requête : {e}")
         return []
 
 
-def fetch_data_from_data_eco_api() -> dict:
+def fetch_datasets_from_data_eco_api() -> dict:
 #def fetch_data_from_data_eco_api(api_url: str) -> dict:
     """
     Récupère les données brutes à partir de l'API de data.economie.gouv.fr
@@ -40,24 +49,63 @@ def fetch_data_from_data_eco_api() -> dict:
     }
 
     while True:
-        response = requests.get(api_url, params=params)
+        response = requests.get(api_url, headers=HEADERS, params=params)
 
         if response.status_code != 200:
             print(f"Error fetching data. Status code: {response.status_code}")
             return {}
 
+        # Récupération et ajout des résultats
         data = response.json()
         all_data.extend(data["results"])
 
+        # COndition de sortie
         if len(data["results"]) < 100:  # Si le nombre d'éléments récupérés est inférieur à la limite par page, cela signifie que nous avons récupéré tous les éléments disponibles
             break
 
         params["offset"] += 100  # Déplacer l'offset pour récupérer la page suivante
 
-        # Sauvegarde les discussions formatées au format JSON
-        dump_json("all_data_eco_datasets.json", all_data)
+    # Sauvegarde les discussions formatées au format JSON
+    dump_json("all_data_eco_datasets.json", all_data)
 
     return all_data
+
+
+def fetch_discussions_from_data_eco_api() -> dict:
+    """
+    Récupère les données brutes à partir de l'API de data.economie.gouv.fr
+    """
+    api_url = "https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/interne-discussions/records"
+
+    all_data = []
+    limit = 100  # Le nombre de résultats par page (ajustez selon la documentation de l'API)
+    offset = 0
+    total_count = 0
+    
+    try:
+        while True:
+            response = requests.get(api_url, headers=HEADERS, params={"limit": limit, "offset": offset})
+            response.raise_for_status()
+            data_json = response.json()
+            total_count = data_json["total_count"]
+            records = data_json.get('results', [])
+            
+            if not records:
+                break
+            
+            all_data.extend(records)
+            offset += limit
+        
+        # Sauvegarde les discussions formatées au format JSON
+        dump_json("all_data_eco_discussions.json", {"records": all_data})
+        return {"records": all_data, "total_count": total_count}
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+    return None
+
 
 def format_data(data_json, api_type):
     """
@@ -109,7 +157,7 @@ def main(api_type):
         return
     
     # Récupère les données depuis l'API
-    data_json = fetch_data_from_data_gouv_api() if api_type == "data_gouv" else fetch_data_from_data_eco_api()
+    data_json = fetch_discussions_from_data_gouv_api() if api_type == "data_gouv" else get_discussions_from_data_eco_api()
     
     # Formate les données pour l'exportation CSV
     formatted_data = format_data(data_json, api_type)
