a8a7996574a5eb732f247c22ba20cc26c92150e6
Asma RACHIDI <rachidi.asm@gmail.com>
2024-08-05 15:16:12 +0200

    feat(repository): add additional CRUD operations to AbstractCommentRepository

--- domain/gateways.py
+++ domain/gateways.py
@@ -1,12 +1,24 @@
 import abc
+from typing import List
 from domain.models import Message
-
 
 class AbstractCommentRepository(abc.ABC):
     @abc.abstractmethod
     def get_message_by_sk(self, discussion_id: str) -> Message:
-        pass  # pragma: no cover
+        pass
 
     @abc.abstractmethod
     def create_message(self, message: Message) -> None:
-        pass  # pragma: no cover
+        pass
+
+    @abc.abstractmethod
+    def update_message(self, discussion_id: str, updated_message: dict) -> None:
+        pass
+
+    @abc.abstractmethod
+    def delete_message(self, discussion_id: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def get_all_messages(self) -> List[Message]:
+        pass
