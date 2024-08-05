c5feaddfbd4b83cc3868be6be5c4e359b14449f0
Asma RACHIDI <rachidi.asm@gmail.com>
2024-08-05 15:14:39 +0200

    refactor(fetchers): update API fetcher manager to use URLs from config

--- infrastructure/services/api_fetcher_manager.py
+++ infrastructure/services/api_fetcher_manager.py
@@ -1,18 +1,14 @@
-# infrastructure/services/api_fetcher_manager.py
+# Gestion des Fetchers
 from api.fetch_data import BaseFetcher, DataGouvFetcher, DataEcoFetcher
+from core.config import Config
 
 class APIFetcherManager:
     @staticmethod
     def get_client(api_type: str) -> BaseFetcher:
         if api_type == "data_gouv":
-            discussions_url = "https://www.data.gouv.fr/api/1/organizations/ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique/discussions"
-            datasets_url = "https://www.data.gouv.fr/api/1/organizations/ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique/datasets"
-            return DataGouvFetcher(discussions_url, datasets_url)
-        
+            return DataGouvFetcher(Config.DATA_GOUV_DISCUSSIONS_URL, Config.DATA_GOUV_DATASETS_URL)
         elif api_type == "data_eco":
-            discussions_url = "https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/interne-discussions/records"
-            datasets_url = "https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets"
-            return DataEcoFetcher(discussions_url, datasets_url)
-        
+            return DataEcoFetcher(Config.DATA_ECO_DISCUSSIONS_URL, Config.DATA_ECO_DATASETS_URL)
         else:
             raise ValueError(f"No client available for API type: {api_type}")
+
