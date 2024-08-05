acedee8c568a4d4a0ced1eabafff5f872fd93bd6
Asma RACHIDI <rachidi.asm@gmail.com>
2024-08-05 15:11:17 +0200

    feat(repository): add TinyDBCommentRepository for managing messages and handle CRUD operations

---
+++ infrastructure/repositories/tinydb_comment_repository.py
@@ -0,0 +1,27 @@
+from typing import List, Dict
+from tinydb import TinyDB, Query
+from domain.gateways import AbstractCommentRepository
+from domain.models import Message
+from core.config import Config
+
+class TinyDBCommentRepository(AbstractCommentRepository):
+    def __init__(self):
+        self.db = TinyDB(Config.TINYDB_PATH)
+        self.messages_table = self.db.table('messages')
+
+    def get_message_by_sk(self, discussion_id: str) -> Message:
+        result = self.messages_table.get(Query().discussion_id == discussion_id)
+        return Message(**result) if result else None
+
+    def create_message(self, message: Message) -> None:
+        if not self.get_message_by_sk(message.discussion_id): # Pour éviter les duplications
+            self.messages_table.insert(message.__dict__)
+
+    def update_message(self, discussion_id: str, updated_message: Dict) -> None:
+        self.messages_table.update(updated_message, Query().discussion_id == discussion_id)
+
+    def delete_message(self, discussion_id: str) -> None:
+        self.messages_table.remove(Query().discussion_id == discussion_id)
+
+    def get_all_messages(self) -> List[Message]:
+        return [Message(**item) for item in self.messages_table.all()]
