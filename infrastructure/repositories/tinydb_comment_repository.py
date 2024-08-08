6bfb762621a981d639f24f6f90ff532c134b184c
Asma RACHIDI <rachidi.asm@gmail.com>
2024-08-08 12:36:58 +0200

    refactor(repository): improve "Query()" clarity in the methods

--- infrastructure/repositories/tinydb_comment_repository.py
+++ infrastructure/repositories/tinydb_comment_repository.py
@@ -6,24 +6,27 @@ from core.config import Config
 
 class TinyDBCommentRepository(AbstractCommentRepository):
     def __init__(self):
-        self.db = TinyDB(Config.TINYDB_PATH)
+        self.db = TinyDB(Config.TINYDB_PATH) # db = TinyDB('db.json')
         self.messages_table = self.db.table('messages')
 
     def get_message_by_sk(self, discussion_id: str) -> Message:
-        result = self.messages_table.get(Query().discussion_id == discussion_id)
+        MessageQuery = Query()
+        result = self.messages_table.get(MessageQuery.discussion_id == discussion_id)
         return Message(**result) if result else None
 
     def create_message(self, message: Message) -> None:
-        if not self.get_message_by_sk(message.discussion_id): # Pour éviter les duplications
+        if not self.get_message_by_sk(message.discussion_id):
             self.messages_table.insert(message.__dict__)
         else:
             print(f"Duplicate message found for discussion_id: {message.discussion_id}")
 
     def update_message(self, discussion_id: str, updated_message: Dict) -> None:
-        self.messages_table.update(updated_message, Query().discussion_id == discussion_id)
+        MessageQuery = Query()
+        self.messages_table.update(updated_message, MessageQuery.discussion_id == discussion_id)
 
     def delete_message(self, discussion_id: str) -> None:
-        self.messages_table.remove(Query().discussion_id == discussion_id)
+        MessageQuery = Query()
+        self.messages_table.remove(MessageQuery.discussion_id == discussion_id)
 
     def get_all_messages(self) -> List[Message]:
         return [Message(**item) for item in self.messages_table.all()]
