cd844cd2cc4b080bdee2bd5a5d822f13cfb1a7f0
Asma RACHIDI <rachidi.asm@gmail.com>
2024-08-08 16:07:13 +0200

    refactor(db_path): changed db path

--- core/config.py
+++ core/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    TINYDB_PATH = os.getenv('TINYDB_PATH', 'db.json')
+    TINYDB_PATH = os.getenv('TINYDB_PATH', 'data/raw/tinydb.json')
     API_KEY = os.getenv('API_KEY')
     DATA_GOUV_DISCUSSIONS_URL = "https://www.data.gouv.fr/api/1/organizations/ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique/discussions"
     DATA_GOUV_DATASETS_URL = "https://www.data.gouv.fr/api/1/organizations/ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique/datasets"
