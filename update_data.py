e7b2e7bdce6f8fd0b577c5bf93d2e70355262244
Asma RACHIDI <rachidi.asm@gmail.com>
2024-08-08 16:08:24 +0200

    feat(update_data): add script for updating data (fetch and store data to tinydb)

--- update_data.py
+++ update_data.py
@@ -1,18 +1,22 @@
+import logging
 from infrastructure.repositories.tinydb_comment_repository import TinyDBCommentRepository
 from domain.usecases import FetchAndStoreDiscussions
+from core.config import Config
 
-repository = TinyDBCommentRepository()
-
-fetch_and_store_data_gouv_discussions = FetchAndStoreDiscussions(repository, "data_gouv")
-fetch_and_store_data_eco_discussions = FetchAndStoreDiscussions(repository, "data_eco")
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def update_data():
-    fetch_and_store_data_gouv_discussions.execute()
-    fetch_and_store_data_eco_discussions.execute()
+    repository = TinyDBCommentRepository()
+
+    logging.info("Fetching and storing discussions from data_gouv...")
+    fetch_and_store_data_gouv = FetchAndStoreDiscussions(repository, "data_gouv")
+    fetch_and_store_data_gouv.execute()
+    logging.info("Data from data_gouv fetched and stored successfully.")
+
+    logging.info("Fetching and storing discussions from data_eco...")
+    fetch_and_store_data_eco = FetchAndStoreDiscussions(repository, "data_eco")
+    fetch_and_store_data_eco.execute()
+    logging.info("Data from data_eco fetched and stored successfully.")
 
 if __name__ == "__main__":
     update_data()
-
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# Mettre à jour les données quotidiennement avec crontab.
