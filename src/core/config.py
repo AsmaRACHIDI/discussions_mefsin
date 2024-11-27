470db41d301778a5cf102d55d5041ca0e935b81c
Asma RACHIDI <rachidi.asm@gmail.com>
2024-11-27 15:46:23 +0100

    refactor(config): rename MEFSIN to MEFI and add env var for organization name

--- src/core/config.py
+++ src/core/config.py
@@ -7,12 +7,19 @@ os.makedirs("data/raw", exist_ok=True)
 # os.makedirs("trained_models/", exist_ok=True)
 
 class Config:
+    # Chargement des variables d'environnement
     TINYDB_PATH = os.getenv("TINYDB_PATH", "data/raw/tinydb.json")
 
     API_KEY = os.getenv("API_KEY")
+    
+    organization_name = os.getenv("ORGANIZATION_NAME")
 
-    DATA_GOUV_DISCUSSIONS_URL = "https://www.data.gouv.fr/api/1/organizations/ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique/discussions"
-    DATA_GOUV_DATASETS_URL = "https://www.data.gouv.fr/api/1/organizations/ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique/datasets"
+    #DATA_GOUV_DISCUSSIONS_URL = "https://www.data.gouv.fr/api/1/organizations/ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique/discussions"
+    #DATA_GOUV_DATASETS_URL = "https://www.data.gouv.fr/api/1/organizations/ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique/datasets"
+    
+    DATA_GOUV_DISCUSSIONS_URL = f"https://www.data.gouv.fr/api/1/organizations/{organization_name}/discussions"
+    DATA_GOUV_DATASETS_URL = f"https://www.data.gouv.fr/api/1/organizations/{organization_name}/datasets"
+
 
     DATA_ECO_DISCUSSIONS_URL = (
         "https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/interne-discussions/records"
